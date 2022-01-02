1a9257f326906be03f40ee555a6677b50a6df192
Luderio Sanchez <luderio.rubis.sanchez@gmail.com>
2022-01-03 06:57:37 +0800

    Initial Commit

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -5,5 +5,13 @@ const ConvertHandler = require('../controllers/convertHandler.js');
 let convertHandler = new ConvertHandler();
 
 suite('Unit Tests', function(){
+    suite('Input Validation', function() {
+        //for whole number input
+        test('Whole Number Input', function() {
 
+            let convertHandler = new ConvertHandler();
+
+            assert.isNumber(convertHandler.getNum, 'Error: input must be a number');
+        });
+    });
 });

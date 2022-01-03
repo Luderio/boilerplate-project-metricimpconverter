834fcc1207017f2a69a7154b6e08c26231c0e3b8
Luderio Sanchez <luderio.rubis.sanchez@gmail.com>
2022-01-03 17:51:35 +0800

    Output

--- routes/api.js
+++ routes/api.js
@@ -15,11 +15,7 @@ module.exports = function (app) {
     let returnUnit = convertHandler.getReturnUnit(initUnit);
     let toString = convertHandler.getString(initNum, initUnit, returnNum, returnUnit);
 
-    //console.log(initNum + " " + convertHandler.spellOutUnit(initUnit) + " converts to " + returnNum + " " + convertHandler.spellOutUnit(returnUnit));
-    //console.log(JSON.stringify(toString));
-
-    document.getElementById('result').innerHTML = initNum + " " + convertHandler.spellOutUnit(initUnit) + " converts to " + returnNum + " " + convertHandler.spellOutUnit(returnUnit)
-    document.getElementById('jsonResult').innerHTML = JSON.stringify(toString);
+    response.json(toString)
   });
 
   

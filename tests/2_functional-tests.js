a88ba7eeff90f607490a275b4786ad4fd7b8a321
Luderio Sanchez <luderio.rubis.sanchez@gmail.com>
2022-01-04 13:36:59 +0800

    Functional tests

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -7,29 +7,32 @@ chai.use(chaiHttp);
 
 suite('Functional Tests', function() {
 
-    suite('GET /api/convert => conversion object', function() {
+    suite('Routing Tests', function() {
 
-        test('Convert 10L (valid input)', function(done) {
-            chai.request(server)
-            .get('/api/convert')
-            .query({input: '10L'})
-            .end(function(err, res) {
-                assert.equal(res.status, 200);
-                assert.equal(res.body.initNum, 10);
-                assert.equal(res.body.initUnit, 'L');
-                assert.approximately(res.body.returnNum, 2.64172, 0.1);
-                assert.equal(res.body.returnUnit, 'gal');
-                done();
+        suite('GET /api/convert => conversion object', function() {
+
+            test('Convert 10L (valid input)', function(done) {
+                chai.request(server)
+                .get('/api/convert')
+                .query({input: '10L'})
+                .end(function(err, res) {
+                    assert.equal(res.status, 200);
+                    assert.equal(res.body.initNum, 10);
+                    assert.equal(res.body.initUnit, 'L');
+                    assert.approximately(res.body.returnNum, 2.64172, 0.1);
+                    assert.equal(res.body.returnUnit, 'gal');
+                    done();
+                });
             });
-        });
-
-        test('Convert 32g (invalid input unit)', function(done) {
-            chai.request(server)
-            .get('/api/convert')
-            .query({input: '32g'})
-            .end(function(err, res) {
-                assert.equal(res.body, 'invalid unit');
-                done();
+    
+            test('Convert 32g (invalid input unit)', function(done) {
+                chai.request(server)
+                .get('/api/convert')
+                .query({input: '32g'})
+                .end(function(err, res) {
+                    assert.equal(res.body, 'invalid unit');
+                    done();
+                });
             });
         });
     });

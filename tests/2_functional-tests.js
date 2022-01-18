d0d32a77fdf7438f07025732b4f9e07dd12e8a3e
Luderio Sanchez <luderio.rubis.sanchez@gmail.com>
2022-01-18 11:29:03 +0800

    functional test updated

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -44,6 +44,34 @@ suite('Functional Tests', function() {
                     done();
                 });
             });
+
+            test('Convert an invalid number AND unit such as 3/7.2/4kilomegagram (invalid number and unit)', function(done) {
+                chai.request(server)
+                .get('/api/convert')
+                .query({input: '3/7.2/4kilomegagram'})
+                .end(function(err, res) {
+                    assert.equal(res.body, 'invalid number');
+                    done();
+                });
+            });
+
+            test('Convert with no number such as kg (default to 1)', function(done) {
+                chai.request(server)
+                .get('/api/convert')
+                .query({input: 'kg'})
+                .end(function(err, res) {
+                    assert.equal(res.status, 200);
+                    assert.equal(res.body.initNum, 1);
+                    assert.equal(res.body.initUnit, 'kg');
+                    assert.approximately(res.body.returnNum, 2.20462, 0.1);
+                    assert.equal(res.body.returnUnit, 'pounds');
+                    done();
+                });
+            });
+
+
+
+
         });
     });
 
